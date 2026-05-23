deef46847a09b105c6594d6f4b895a48fb4f3e3d
Shyam Kalairajah <shyamkalairajah@shyams-air.home>
2026-05-23 17:03:53 +0100

    milestone 1: POST /charges with validation and uuid

--- main.py
+++ main.py
@@ -1,7 +1,27 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
+from typing import Optional
+from pydantic import Field 
+import uuid
+
 
 app = FastAPI()
+
+class ChargeRequest(BaseModel):
+    # your fields go here
+    amount: float = Field(gt=0.01)
+    currency: str
+    payment_method: str
+    description: Optional[str] = None
 
 @app.get("/health")
 def health_check():
     return {"status": "ok"}
+
+@app.post("/charges", status_code=201)
+def charges(charge: ChargeRequest):
+    id = uuid.uuid4()
+    return {"charge_id": id}
+
+
+
